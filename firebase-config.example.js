3b2212609ea4cb76526d4f6dfbf1dbb3b93c736c
nnurallar-commits <nnurallar@gmail.com>
2026-08-27 14:21:19 +0300

    Update firebase-config.example.js

--- firebase-config.example.js
+++ firebase-config.example.js
@@ -1,9 +1,9 @@
-// Bunu firebase-config.js adıyla kopyala ve Firebase Console > Project settings > Web app değerlerini yapıştır.
-export const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+window.ELOG_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA642NY42_ospJmzhYpiBQt6HcEK_JOt4w",
+  authDomain: "e-log-2f316.firebaseapp.com",
+  projectId: "e-log-2f316",
+  storageBucket: "e-log-2f316.firebasestorage.app",
+  messagingSenderId: "846487456126",
+  appId: "1:846487456126:web:2fa3c565b7bde7aae20b2f",
+  measurementId: "G-7Z8VDCFT4J"
 };
